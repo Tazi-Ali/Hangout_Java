13daf3b82f906fe06f0fb859741396149a459f21
zohair-aabidi <zohair.aabidi@emsi-edu.ma>
2025-05-12 01:48:40 +0100

    Initial commit

---
+++ backend/hangout/src/test/java/com/hangoutjava/hangout/HangoutApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hangoutjava.hangout;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HangoutApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
